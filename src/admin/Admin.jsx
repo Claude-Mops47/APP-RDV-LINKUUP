408b2a6dce80c7d116aa44e775f107631e38bda9
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-06-07 16:49:05 +0100

    filter agent

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { appointmentActions } from "_store";
 import moment from "moment";
 
@@ -15,6 +15,66 @@ function Admin() {
     dispatch(appointmentActions.getAllAppointments());
   }, []);
 
+  const downloadAsCSV = () => {
+    // Create a CSV string from the appointment data
+    const csvContent = [
+      "Agent,Date,Name,Telephone,Address,Date programmation,Commercial",
+      ...appointments?.value?.map((item) => {
+        const phoneNumbers = Array.isArray(item.phone)
+          ? item.phone.join(" ").split("/")
+          : null;
+        const agent = item.posted_by?.firstName;
+        const dateStr = item.date;
+        const dateObj = moment(dateStr);
+        const formattedDate = dateObj.format("DD-MM-YY [à] HH:mm");
+        const address = item.address;
+        const commercial = item.commercial;
+        return [
+          agent,
+          formattedDate,
+          item.name,
+          phoneNumbers ? phoneNumbers.join(", ") : "",
+          address,
+          formattedDate,
+          commercial,
+        ].join(",");
+      }),
+    ].join("\n");
+
+    // Create a Blob object with the CSV content
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+
+    // Create a temporary anchor element to trigger the download
+    const link = document.createElement("a");
+    if (link.download !== undefined) {
+      const url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", "appointments.csv");
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+  const [filterAgent, setFilterAgent] = useState("");
+
+  const handleAgentFilterChange = (e) => {
+    setFilterAgent(e.target.value);
+  };
+
+  const filteredAppointments = appointments?.value?.filter((item) => {
+    if (
+      filterAgent &&
+      !item.posted_by?.firstName
+        .toLowerCase()
+        .includes(filterAgent.toLowerCase())
+    ) {
+      return false;
+    }
+    return true;
+  });
+
   return (
     <div>
       <h1>Hi {auth?.firstName}!</h1>
@@ -23,8 +83,23 @@ function Admin() {
         <Link to="/users">Manage Users</Link>
       </p>
 
+      <button className="btn btn-link" onClick={downloadAsCSV}>
+        Download appointment list as CSV
+      </button>
+
       <div className="table-wrapper">
         <h2>Liste des rendez-vous</h2>
+        <div className="form-group">
+          <label htmlFor="agentFilter">Filter par agent :</label>
+          <input
+            className="input-control"
+            type="text"
+            id="agentFilter"
+            value={filterAgent}
+            onChange={handleAgentFilterChange}
+          />
+        </div>
+        <br />
 
         <table className="fl-table">
           <thead>
@@ -40,8 +115,9 @@ function Admin() {
             </tr>
           </thead>
           <tbody>
-            {appointments?.value?.map((item, index) => {
+            {filteredAppointments?.map((item, index) => {
               // phone array
+
               const phoneNumbers = Array.isArray(item.phone)
                 ? item.phone.join(" ").split("/")
                 : null;
@@ -54,7 +130,7 @@ function Admin() {
               // date created
               const dateCreated = item.posted_by?.createdAt;
               const dateObj2 = moment(dateCreated);
-              const formattedDateCreated = dateObj2.format("DD-MM-YY");
+              const formattedDateCreated = dateObj2.format("DD-MMMM");
 
               return (
                 <tr key={item._id}>
